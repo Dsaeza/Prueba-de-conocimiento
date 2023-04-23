282ca1c8e436f4a85785f5c46187bbc6d28d8671
Dsaeza <88747468+Dsaeza@users.noreply.github.com>
2023-04-22 22:03:25 -0400

    Creación del proyecto

---
+++ app/src/main/java/com/testt/conocimiento/MainActivity.java
@@ -0,0 +1,13 @@
+package com.testt.conocimiento;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedIntanceState){
+        super.onCreate(savedIntanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
